6f4c289f559deb5e34f53e3a2bf899ce6f78b0f1
Jon Vuri <jrajav@gmail.com>
2024-01-23 00:36:11 -0600

    Implement syncedstore's todo list demo in app

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,75 @@
-import styles from './page.module.css'
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { useSyncedStore } from '@syncedstore/react'
 
 import YoutubeControl from './components/YoutubeControl'
+import { store } from './store'
+
+import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>Player:</p>
-      </div>
+  // The store will load the initial state faster than the first render,
+  // so React will complain the first client render doesn't match the server's
+  // render. We mount it after the first render with an effect to avoid this.
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
 
-      <div className={styles.center}>
-        <YoutubeControl />
-      </div>
-    </main>
+  const state = useSyncedStore(store)
+
+  return (
+    mounted && (
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>Player:</p>
+        </div>
+
+        <div className={styles.center}>
+          <YoutubeControl />
+        </div>
+
+        <div>
+          <p>Todo items:</p>
+          <ul>
+            {state.todos.map((todo, i) => {
+              return (
+                <li
+                  key={i}
+                  style={{
+                    textDecoration: todo.completed ? 'line-through' : '',
+                  }}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={todo.completed}
+                      onChange={(event) => {
+                        todo.completed = event.target.checked
+                      }}
+                    />
+                    {todo.title}
+                  </label>
+                </li>
+              )
+            })}
+          </ul>
+          <input
+            placeholder="Enter a todo item and hit enter"
+            type="text"
+            onKeyPress={(event) => {
+              if (event.key === 'Enter') {
+                const target = event.target as HTMLInputElement
+                // Add a todo item using the text added in the textfield
+                state.todos.push({ completed: false, title: target.value })
+                target.value = ''
+              }
+            }}
+            style={{ width: '200px', maxWidth: '100%' }}
+          />
+        </div>
+      </main>
+    )
   )
 }
